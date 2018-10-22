236ef581bf81e4e7131f55217a1c9c4443a46161
Michael Elderhorst <michael.elderhorst@gmail.com>
2018-10-22 00:02:37 -0700

    Implementing the most basic of command handling

--- src/mud/client.rs
+++ src/mud/client.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 
 use commands::commands;
 use game::player::Player;
-use mud::mudserver::MudServer;
+use mud::updatedata::UpdateData;
 
 pub struct Client {
     id: usize,
@@ -26,26 +26,41 @@ impl Client {
         self.stream.write(response.as_bytes()).expect("Response failed");
     }
 
-    pub fn update(&mut self) {
+    pub fn update(&mut self) -> UpdateData {
         let mut buffer = String::new();
         let result = self.stream.read_to_string(&mut buffer);
         buffer = buffer.trim().to_string();
 
         if buffer.len() != 0 {
-            self.process_input(buffer);
+            return self.process_input(buffer);
+        }
+        else {
+            return UpdateData {
+                id: self.id,
+                command: "".to_string(),
+                params: "".to_string(),
+            };
         }
 
         // There is probably an error this should handle.
-        match result {
+        /*match result {
             Ok(_) => {},
             _ => {},
-        }
+        }*/
     }
 
-    fn process_input(&mut self, mut text: String) {
-        let params = text.split_off(7);
+    fn process_input(&mut self, mut text: String) -> UpdateData {
+        let data = UpdateData {
+            id: self.id.clone(),
+            command: "help".to_string(),
+            params: "".to_string(),
+        };
+        
+        let params = "".to_string();//text.split_off(7);
 
-
+        // TODO: MOVE THIS
         commands::handle_command(self, &text, &params);
+
+        return data;
     }
 }
--- src/mud/mudserver.rs
+++ src/mud/mudserver.rs
@@ -2,39 +2,38 @@ use std::net::TcpStream;
 use std::vec::Vec;
 
 use mud::client::Client;
+use mud::serverdata::ServerData;
+use mud::updatedata::UpdateData;
 use game::game::Game;
 
-//#[derive(Copy, Clone)]
 pub struct MudServer {
-    pub clients: Vec<Client>,
     pub next_id: usize,
     pub game: Game,
+    pub server_data: ServerData,
 }
 
 impl MudServer {
     pub fn new() -> MudServer {
         MudServer {
-            clients: Vec::new(),
             next_id: 0,
             game: Game::new(),
+            server_data: ServerData::new(),
         }
     }
 
     pub fn add_client(&mut self, stream: TcpStream) -> usize {
         let client = Client::new(self.next_id, stream);
-        self.clients.push(client);
+        self.server_data.add_client(client);
 
         self.next_id += 1;
         self.next_id.clone()
     }
 
     pub fn send_message(&mut self, id: usize, text: String) {
-        self.clients[id].send(text);
+        self.server_data.clients[id].send(text);
     }
 
     pub fn update(&mut self) {
-        for id in 0..self.clients.len() {
-            self.clients[id].update();
-        }
+        let data = self.server_data.update_clients();
     }
 }
