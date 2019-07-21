e357bd5bcffeacc21c3c89ba43fbb4e1fc8c60ca
Michael Elderhorst <michael.elderhorst@gmail.com>
2019-07-21 16:16:13 -0700

    Fixed database table creation and updated SQLite

--- src/mud/database.rs
+++ src/mud/database.rs
@@ -24,7 +24,7 @@ impl Database {
     fn setup_table(&mut self) {
         self.conn.execute(
             "CREATE TABLE IF NOT EXISTS players (
-                    name            INTEGER UNIQUE PRIMARY KEY,
+                    name            TEXT PRIMARY KEY,
                     password        TEXT NOT NULL,
                     room            TEXT NOT NULL,
                     str             INT NOT NULL,
@@ -47,24 +47,27 @@ impl Database {
         let mut stmt = self.conn
         .prepare(&command)
         .unwrap();
+
         let player_iter = stmt
-            .query_map(NO_PARAMS, |row| Player {
-                name: row.get(0),
-                password: row.get(1),
-                room: row.get(2),
-                abilities: Abilities::remake(row.get(3), row.get(4), row.get(5), row.get(6), row.get(7), row.get(8)),
-                race: row.get(9),
-                health: row.get(10),
-                max_health: row.get(11),
-                level: row.get(12),
-                inventory: row.get(13),
-                status: Status::new(),
-            }).unwrap();
+            .query_map(NO_PARAMS, |row|
+                Ok (
+                    Player {
+                        name: row.get(0).unwrap(),
+                        password: row.get(1).unwrap(),
+                        room: row.get(2).unwrap(),
+                        abilities: Abilities::remake(row.get(3).unwrap(), row.get(4).unwrap(), row.get(5).unwrap(), row.get(6).unwrap(), row.get(7).unwrap(), row.get(8).unwrap()),
+                        race: row.get(9).unwrap(),
+                        health: row.get(10).unwrap(),
+                        max_health: row.get(11).unwrap(),
+                        level: row.get(12).unwrap(),
+                        inventory: row.get(13).unwrap(),
+                        status: Status::new(),
+            })).unwrap();
 
         let mut players = Vec::new();
 
-        for plyrs in player_iter {
-            players.push(plyrs.unwrap());
+        for player in player_iter {
+            players.push(player.unwrap());
         }
 
         players
